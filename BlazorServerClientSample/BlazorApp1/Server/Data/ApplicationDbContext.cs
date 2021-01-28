d02af60f4d1fa53f9fb52abc4f72475a393605d1
William Mendoza <support@kryptoit.com>
2021-01-27 21:57:18 -0500

    Adding applicationUser to Identitydbcontext

--- BlazorServerClientSample/BlazorApp1/Server/Data/ApplicationDbContext.cs
+++ BlazorServerClientSample/BlazorApp1/Server/Data/ApplicationDbContext.cs
@@ -19,7 +19,7 @@ namespace BlazorApp1.Server.Data
         {
         }
     }
-    //public class ApplicationDbContext : IdentityDbContext
+    //public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     //{
     //    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
     //        : base(options)
